a24bc8c417e7d314f8694c8b1c8966ccf654b78e
Sainirmit <sainirmit@gmail.com>
2024-06-04 23:34:15 +0530

    Initial Commit

---
+++ PhotosApp/PhotosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotosAppApp.swift
+//  PhotosApp
+//
+//  Created by Sai Nirmit on 04/06/24.
+//
+
+import SwiftUI
+
+@main
+struct PhotosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
